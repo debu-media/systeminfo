8c463787c2fcd506a226d04968ad729a4a064ca7
Patryk <36153996+debu-media@users.noreply.github.com>
2018-09-13 08:47:22 +0200

    Version 1.1
    new text color 
    mac and ip adress set to loop

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,30 +45,30 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(2, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(167, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nazwa komputera : ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(164, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(167, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nazwa komputera : ";
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(2, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 20);
@@ -78,20 +78,21 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(81, 44);
             this.label4.Name = "label4";
             this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label4.Size = new System.Drawing.Size(88, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Adres IP :";
+            this.label4.MouseHover += new System.EventHandler(this.label4_MouseHover);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(2, 75);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(109, 20);
@@ -101,8 +102,8 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(104, 75);
             this.label6.Name = "label6";
             this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -114,7 +115,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.ForeColor = System.Drawing.Color.White;
             this.label7.Location = new System.Drawing.Point(2, 108);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(39, 20);
@@ -124,8 +125,8 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.ForeColor = System.Drawing.Color.Lime;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.Color.White;
             this.label8.Location = new System.Drawing.Point(46, 108);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(39, 20);
@@ -136,7 +137,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label9.ForeColor = System.Drawing.Color.Lime;
+            this.label9.ForeColor = System.Drawing.Color.White;
             this.label9.Location = new System.Drawing.Point(2, 144);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(52, 20);
@@ -146,8 +147,8 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label10.ForeColor = System.Drawing.Color.Lime;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label10.ForeColor = System.Drawing.Color.White;
             this.label10.Location = new System.Drawing.Point(60, 144);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(52, 20);
@@ -158,7 +159,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.BackColor = System.Drawing.Color.Gray;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(373, 267);
             this.ControlBox = false;
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,12 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace WindowsFormsApp1
 {
+
+    
     public partial class head : Form
     {
         public head()
@@ -20,61 +23,120 @@ namespace WindowsFormsApp1
             InitializeComponent();
             this.StartPosition = FormStartPosition.Manual;
         }
+        public void dajadresip(object sender, EventArgs e)
 
-        private void Form1_Load(object sender, EventArgs e)
+        { adresip();
+        }
+         public void dajmacadres(object sender, EventArgs e)
+
         {
-            TransparencyKey = Color.Fuchsia;
-            Rectangle res = Screen.PrimaryScreen.Bounds;
-            this.Location = new Point(res.Width-Size.Width,50);
-            label2.Text = System.Environment.MachineName;
+            macadres();
+        }
+
+        private void macadres()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+
+                if (nic.OperationalStatus == OperationalStatus.Up && (!nic.Description.Contains("Virtual") && !nic.Description.Contains("Pseudo") && !nic.Description.Contains("Bluetooth") && !nic.Description.Contains("Device") && !nic.Description.Contains("Urządzenie") && !nic.Description.Contains("DW5811e")))
+                {
+                    if (nic.GetPhysicalAddress().ToString() != "")
+                    {
+                        string mac = Convert.ToString(nic.GetPhysicalAddress());
+                        string splitmac = mac;
+                        int insertCount = 0;
+                        for (int i = 2; i < mac.Length; i = i + 2)
+                        {
+                            splitmac = splitmac.Insert(i + insertCount++, ":");
+                        }
+                        label10.Text = splitmac;
+
+                    }
+                }
+            }
+        }
+
+        public void adresip() { 
             label4.Text = System.Net.Sockets.AddressFamily.InterNetwork.ToString();
             foreach (IPAddress currentIPAddress in Dns.GetHostAddresses(Dns.GetHostName()))
             {
                 Byte[] tablica = currentIPAddress.GetAddressBytes();
-                if (tablica[0] == 192 && tablica[1] == 168)
+                if (tablica[0] == 192 && tablica[1] == 168) // Wyciągnij adres tylko z tej puli. 
                 {
                     if (currentIPAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
                     { label4.Text = currentIPAddress.ToString(); }
-                }
+    }
                 else {
                     continue;
-                }
+                
             }
-
+ }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            adresip();
+            macadres();
+            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+            t.Interval = 120000; // odświeżanie co 2 min
+            t.Tick += new EventHandler(dajadresip);
+            t.Start();
+            System.Windows.Forms.Timer m = new System.Windows.Forms.Timer();
+            m.Interval = 120000; // odświeżanie co 2 min
+            m.Tick += new EventHandler(dajmacadres);
+            m.Start();
+            TransparencyKey = Color.Gray; // Wszytko co szare jest transparentne  
+            Rectangle res = Screen.PrimaryScreen.Bounds;
+            this.Location = new Point(res.Width-Size.Width,50);
+            label2.Text = System.Environment.MachineName;
             label6.Text = Environment.UserDomainName + "\\" + Environment.UserName;
             label8.Text = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName", "").ToString();
+            if  (label8.Text== "Windows 10 Enterprise") { label8.Text = "Windows 10 Pro"; }
             if (label8.Text == "Windows 10 Pro")
             {
                 label8.Text =  label8.Text + " wersja: " + Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ReleaseId", "").ToString();
             }
             
-            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
-            {
-
-                if (nic.OperationalStatus == OperationalStatus.Up && (!nic.Description.Contains("Virtual") && !nic.Description.Contains("Pseudo") && !nic.Description.Contains("Bluetooth") && !nic.Description.Contains("Device") && !nic.Description.Contains("Urządzenie") &&  !nic.Description.Contains("DW5811e")))
-                {
-                    if (nic.GetPhysicalAddress().ToString() != "")
-                    {
-                       string mac = Convert.ToString(nic.GetPhysicalAddress());
-                        string splitmac = mac;
-                        int insertCount = 0;
-                        for (int i = 2; i < mac.Length; i=i+2)
-                        {
-                            splitmac = splitmac.Insert(i + insertCount++, ":");
-                        }
-                        label10.Text = splitmac;
-                       
-                    }
-                }
-            }
+               
+         
+          
             
 
         }
 
        
-        private void label1_Click(object sender, EventArgs e)
-        {
 
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+             // Zamiana koloru napisów po najechaniu na czarne 
+            label1.ForeColor = Color.Black;
+            label2.ForeColor = Color.Black;
+            label3.ForeColor = Color.Black;
+            label4.ForeColor = Color.Black;
+            label5.ForeColor = Color.Black;
+            label6.ForeColor = Color.Black;
+            label7.ForeColor = Color.Black;
+            label8.ForeColor = Color.Black;
+            label9.ForeColor = Color.Black;
+            label10.ForeColor = Color.Black;
+        }
+        private void label4_MouseHover(object sender, EventArgs e)
+        {
+            // Zamiana koloru napisów po najechaniu na czerwone 
+            label1.ForeColor = Color.Red;
+            label2.ForeColor = Color.Red;
+            label3.ForeColor = Color.Red;
+            label4.ForeColor = Color.Red;
+            label5.ForeColor = Color.Red;
+            label6.ForeColor = Color.Red;
+            label7.ForeColor = Color.Red;
+            label8.ForeColor = Color.Red;
+            label9.ForeColor = Color.Red;
+            label10.ForeColor = Color.Red;
         }
     }
-}
+
+
+
+
+    }
+
